7b5eadeae948734397bb69a55153d9c1507c747e
ameya-6964 <belvalkar7@gmail.com>
2023-01-07 01:36:29 +0530

    Added Nodemon

--- basicHTTPserver/index.js
+++ basicHTTPserver/index.js
@@ -1,13 +1,24 @@
 //! Always Import HTTP Module
 const http = require('http')
 const port = 8000;
+const fs = require('fs')
 
 //? Basic Request and Response Server
 function requestHandler(req,res)
 {
     console.log(req.url);
     res.writeHead(200,{'content-type': 'text/html'})
-    res.end("<h1 style = color:red > Gotchaaa <h1/>");
+
+    fs.readFile('./index.html', function(err,data){
+        if(err)
+        {
+            console.log(`Error ${err}`)
+            return res.end("<h1 style = color:red > Gotchaaa <h1/>");
+        }
+        return res.end(data);
+
+    })
+   //// res.end("<h1 style = color:red > Gotchaaa <h1/>");
 }
 
 //* Create Server Syntax 
@@ -19,5 +30,5 @@ if(err)
     return
 }
 
-console.log(`Server Is Running On Port ${port}`)
+console.log(`Server Is Running ${port}`)
 })
