811cac1f4acecc4f28289dc5c623dce87a78278c
ameya-6964 <belvalkar7@gmail.com>
2023-01-10 01:20:32 +0530

    Rendered First Ejs on Local Host

--- contact-list/index.js
+++ contact-list/index.js
@@ -1,9 +1,12 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const app = express()
+app.set('view engine', 'ejs');
+app.set('views' , path.join(__dirname,'views'))
 const port = 8000
 
 app.get('/', (req, res) => {
-  res.send("<h1> Hey I Am Home Page </h1>")
+  return res.render('home');
 })
 
 app.get('/profile', (req, res) => {
