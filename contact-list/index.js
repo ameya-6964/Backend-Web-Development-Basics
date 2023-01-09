badaec0d31b019b7cde9fd8392fee732af69a42e
ameya-6964 <belvalkar7@gmail.com>
2023-01-10 02:07:10 +0530

    Changes

--- contact-list/index.js
+++ contact-list/index.js
@@ -6,18 +6,8 @@ app.set('views' , path.join(__dirname,'views'))
 const port = 8000
 
 app.get('/', (req, res) => {
-  return res.render('home');
+  return res.render('home' , {title : "My Contact Lists" } );
 })
-
-app.get('/profile', (req, res) => {
-    res.send("<h1> Hey I am Profile Page </h1>")
-  })
-  app.get('/about', (req, res) => {
-    res.send("<h1> Hey I Am About Page </h1>")
-  })
-  app.get('/gallery', (req, res) => {
-    res.send("<h1> Wonderful Photos </h1>")
-  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
