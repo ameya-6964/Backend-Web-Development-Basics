6bf36986862e44ea04f36e9fabe127e1804852ea
ameya-6964 <belvalkar7@gmail.com>
2023-01-14 22:42:24 +0530

    Just For Streak

--- basicHTTPserver/index.js
+++ basicHTTPserver/index.js
@@ -1,35 +1,32 @@
 //! Always Import HTTP Module
-const http = require('http')
+const http = require("http");
 const port = 8000;
 //! File System Module To Read Files
-const fs = require('fs')
+
+const fs = require("fs");
 
 //? Basic Request and Response Server
-function requestHandler(req,res)
-{
-    console.log(req.url);
-    res.writeHead(200,{'content-type': 'text/html'})
+function requestHandler(req, res) {
+  console.log(req.url);
+  res.writeHead(200, { "content-type": "text/html" });
 
-    fs.readFile('./index.html', function(err,data){
-        if(err)
-        {
-            console.log(`Error ${err}`)
-            return res.end("<h1 style = color:red > Gotchaaa <h1/>");
-        }
-        return res.end(data);
-
-    })
-   //// res.end("<h1 style = color:red > Gotchaaa <h1/>");
+  fs.readFile("./index.html", function (err, data) {
+    if (err) {
+      console.log(`Error ${err}`);
+      return res.end("<h1 style = color:red > Gotchaaa <h1/>");
+    }
+    return res.end(data);
+  });
+  //// res.end("<h1 style = color:red > Gotchaaa <h1/>");
 }
 
-//* Create Server Syntax 
+//* Create Server Syntax
 const server = http.createServer(requestHandler);
-server.listen(port,function(err){
-if(err)
-{
+server.listen(port, function (err) {
+  if (err) {
     console.log(err);
-    return
-}
+    return;
+  }
 
-console.log(`Server Is Running ${port}`)
-})
+  console.log(`Server Is Running ${port}`);
+});
