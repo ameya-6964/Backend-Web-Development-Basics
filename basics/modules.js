3de154415462d4f737ad3bcab40909dbf7ed9a32
ameya-6964 <belvalkar7@gmail.com>
2023-02-01 20:50:18 +0530

    Done

--- basics/modules.js
+++ basics/modules.js
@@ -1,6 +1,22 @@
 // Everything Will Be imported From people.js
 
+/* 
+* First Method  
 const xyz = require("./people");
 
 console.log(xyz.ages);
-console.log(xyz.peoples);
+console.log(xyz.peoples); 
+*/
+
+// * Destructuring Method
+
+const { peoples, ages } = require("./people");
+
+console.log(peoples);
+console.log(ages);
+
+// * Some Common Operating System Methods in Node.js
+const os = require("os");
+console.log(os.platform()); //win32
+console.log(os.release()); // 10.0.22621
+console.log(os.type()); // Windows_NT
