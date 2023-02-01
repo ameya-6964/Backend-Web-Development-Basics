8dc36bbac8dc9b211841c243439c615864eaece4
ameya-6964 <belvalkar7@gmail.com>
2023-02-02 01:11:05 +0530

    Done With Basic HTTP Server

--- basicHTTPserver/index.js
+++ basicHTTPserver/index.js
@@ -1,15 +1,32 @@
 //! Always Import HTTP Module
+//! Always Import File System Module
 const http = require("http");
+const fs = require("fs");
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-
-  // setting Headers
   res.setHeader("Content-Type", "text/html");
-  res.write("<h1> Hello World </h1>");
-  res.write("<h2> Hello User Welcome </h2>");
-  res.end();
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+  // Sending an HTML File
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(port, () => {
