24d52741eefdf39b127dd982f5d0e62d2b126f28
ameya-6964 <belvalkar7@gmail.com>
2023-02-02 00:41:09 +0530

    Modified With Es6 Syntax

--- basicHTTPserver/index.js
+++ basicHTTPserver/index.js
@@ -1,32 +1,17 @@
 //! Always Import HTTP Module
 const http = require("http");
 const port = 8000;
-//! File System Module To Read Files
 
-const fs = require("fs");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
 
-//? Basic Request and Response Server
-function requestHandler(req, res) {
-  console.log(req.url);
-  res.writeHead(200, { "content-type": "text/html" });
+  // setting Headers
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1> Hello World </h1>");
+  res.write("<h2> Hello User Welcome </h2>");
+  res.end();
+});
 
-  fs.readFile("./index.html", function (err, data) {
-    if (err) {
-      console.log(`Error ${err}`);
-      return res.end("<h1 style = color:red > Gotchaaa <h1/>");
-    }
-    return res.end(data);
-  });
-  //// res.end("<h1 style = color:red > Gotchaaa <h1/>");
-}
-
-//* Create Server Syntax
-const server = http.createServer(requestHandler);
-server.listen(port, function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.log(`Server Is Running ${port}`);
+server.listen(port, () => {
+  console.log(`Listening On Port ${port}`);
 });
