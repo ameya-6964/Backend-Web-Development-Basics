6a644d1b4aa651b14cca28181e651e12244a8b42
ameya-6964 <belvalkar7@gmail.com>
2023-02-01 20:27:13 +0530

    setTimeout and setInterval in Node,js

---
+++ basics/global.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+  console.log("Hello World From Three Seconds");
+}, 3000);
+
+setTimeout(() => {
+  console.log("Hello World From 1 Seconds");
+}, 1000);
+
+setTimeout(() => {
+  console.log("Hello World From Two Seconds ");
+}, 2000);
+
+const int = setInterval(() => {
+  console.log("Hey I am Running From Interval After Every Five Seconds");
+}, 5000);
+
+//! Set Time Out Function Will Only Run Once
+//! Set Interval Function Will Run After Specified Time Its Non Ending Block Of Code
